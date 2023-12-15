3218f5d8219f70d639ea8ef0210e5a3933c68270
Mattia Manzati <manzati.mattia@gmail.com>
2023-12-15 13:23:00 +0100

    lint fixes

--- packages/cluster-node/src/index.ts
+++ packages/cluster-node/src/index.ts
@@ -1,20 +1,13 @@
-
 export * as PodsHttp from "./PodsHttp.js"
-
 
 export * as ShardManagerClientHttp from "./ShardManagerClientHttp.js"
 
-
 export * as ShardManagerHttp from "./ShardManagerHttp.js"
-
 
 export * as ShardManagerProtocolHttp from "./ShardManagerProtocolHttp.js"
 
-
 export * as ShardingProtocolHttp from "./ShardingProtocolHttp.js"
 
-
 export * as ShardingServiceHttp from "./ShardingServiceHttp.js"
-
 
 export * as StorageFile from "./StorageFile.js"
--- packages/cluster/src/index.ts
+++ packages/cluster/src/index.ts
@@ -1,98 +1,65 @@
-
 export * as Broadcaster from "./Broadcaster.js"
-
 
 export * as ManagerConfig from "./ManagerConfig.js"
 
-
 export * as Message from "./Message.js"
-
 
 export * as MessageId from "./MessageId.js"
 
-
 export * as MessageState from "./MessageState.js"
-
 
 export * as Messenger from "./Messenger.js"
 
-
 export * as Pod from "./Pod.js"
-
 
 export * as PodAddress from "./PodAddress.js"
 
-
 export * as Pods from "./Pods.js"
-
 
 export * as PodsHealth from "./PodsHealth.js"
 
-
 export * as PoisonPill from "./PoisonPill.js"
-
 
 export * as RecipientBehaviour from "./RecipientBehaviour.js"
 
-
 export * as RecipientBehaviourContext from "./RecipientBehaviourContext.js"
-
 
 export * as RecipientType from "./RecipientType.js"
 
-
 export * as Serialization from "./Serialization.js"
-
 
 export * as SerializedEnvelope from "./SerializedEnvelope.js"
 
-
 export * as SerializedMessage from "./SerializedMessage.js"
-
 
 export * as ShardId from "./ShardId.js"
 
-
 export * as ShardManager from "./ShardManager.js"
-
 
 export * as ShardManagerClient from "./ShardManagerClient.js"
 
-
 export * as Sharding from "./Sharding.js"
-
 
 export * as ShardingConfig from "./ShardingConfig.js"
 
-
 export * as ShardingError from "./ShardingError.js"
-
 
 export * as ShardingErrorEntityNotManagedByThisPod from "./ShardingErrorEntityNotManagedByThisPod.js"
 
-
 export * as ShardingErrorEntityTypeNotRegistered from "./ShardingErrorEntityTypeNotRegistered.js"
-
 
 export * as ShardingErrorMessageQueue from "./ShardingErrorMessageQueue.js"
 
-
 export * as ShardingErrorPodNoLongerRegistered from "./ShardingErrorPodNoLongerRegistered.js"
-
 
 export * as ShardingErrorPodUnavailable from "./ShardingErrorPodUnavailable.js"
 
-
 export * as ShardingErrorSendTimeout from "./ShardingErrorSendTimeout.js"
-
 
 export * as ShardingErrorSerialization from "./ShardingErrorSerialization.js"
 
-
 export * as ShardingEvent from "./ShardingEvent.js"
 
-
 export * as ShardingRegistrationEvent from "./ShardingRegistrationEvent.js"
-
 
 export * as Storage from "./Storage.js"
