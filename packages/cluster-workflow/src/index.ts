c40d3479ac73aece7dd7b9568ceb35a09d95fbbe
Mattia Manzati <mattia.manzati@gmail.com>
2024-01-20 22:41:22 +0100

    fix linting

--- packages/cluster-workflow/src/index.ts
+++ packages/cluster-workflow/src/index.ts
@@ -1,18 +1,12 @@
-
 export * as Activity from "./Activity.js"
-
 
 export * as ActivityContext from "./ActivityContext.js"
 
-
 export * as CrashableRuntime from "./CrashableRuntime.js"
-
 
 export * as DurableExecutionEvent from "./DurableExecutionEvent.js"
 
-
 export * as DurableExecutionJournal from "./DurableExecutionJournal.js"
-
 
 export * as DurableExecutionJournalInMemory from "./DurableExecutionJournalInMemory.js"
 
@@ -21,11 +15,8 @@ export * as DurableExecutionJournalInMemory from "./DurableExecutionJournalInMem
  */
 export * as DurableExecutionJournalMssql from "./DurableExecutionJournalMssql.js"
 
-
 export * as DurableExecutionState from "./DurableExecutionState.js"
 
-
 export * as Workflow from "./Workflow.js"
-
 
 export * as WorkflowContext from "./WorkflowContext.js"
