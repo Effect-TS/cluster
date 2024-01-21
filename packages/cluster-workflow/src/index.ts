b4715736ba053e2d89a5a79f73c27143c71d3620
Mattia Manzati <mattia.manzati@gmail.com>
2024-01-21 11:10:35 +0100

    fix lint

--- packages/cluster-workflow/src/index.ts
+++ packages/cluster-workflow/src/index.ts
@@ -1,15 +1,10 @@
-
 export * as Activity from "./Activity.js"
-
 
 export * as ActivityContext from "./ActivityContext.js"
 
-
 export * as CrashableRuntime from "./CrashableRuntime.js"
 
-
 export * as DurableExecutionEvent from "./DurableExecutionEvent.js"
-
 
 export * as DurableExecutionJournal from "./DurableExecutionJournal.js"
 
@@ -23,11 +18,8 @@ export * as DurableExecutionJournalInMemory from "./DurableExecutionJournalInMem
  */
 export * as DurableExecutionJournalMssql from "./DurableExecutionJournalMssql.js"
 
-
 export * as DurableExecutionState from "./DurableExecutionState.js"
 
-
 export * as Workflow from "./Workflow.js"
-
 
 export * as WorkflowContext from "./WorkflowContext.js"
