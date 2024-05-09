2371a6088bd3c9afb7f16462971b43f640e9b25e
Mattia Manzati <mattia.manzati@gmail.com>
2024-05-09 23:19:40 +0200

    fix index

--- packages/cluster-node/src/index.ts
+++ packages/cluster-node/src/index.ts
@@ -6,6 +6,21 @@ export * as PodsRpc from "./PodsRpc.js"
 /**
  * @since 1.0.0
  */
+export * as ShardManagerClientRpc from "./ShardManagerClientRpc.js"
+
+/**
+ * @since 1.0.0
+ */
+export * as ShardManagerProtocol from "./ShardManagerProtocol.js"
+
+/**
+ * @since 1.0.0
+ */
+export * as ShardManagerServiceRpc from "./ShardManagerServiceRpc.js"
+
+/**
+ * @since 1.0.0
+ */
 export * as ShardingProtocol from "./ShardingProtocol.js"
 
 /**
